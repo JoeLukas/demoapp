8ffa3d49a5b8528d9f1a7867974928f73bbcede3
JoeLukas <joseph.lukasiewicz@gmail.com>
2014-12-04 22:22:32 +0000

    back to nitrous

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "h1", "DemoApp"
+    assert_select "h1", "Demo App"
   end
 
   test "should get help" do
